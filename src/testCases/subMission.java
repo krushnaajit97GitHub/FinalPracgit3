8b0fd88a925438039625a91230f06e07368e3491
krushna ch sahoo <krushnaajit97@gmail.com>
2022-08-09 09:30:51 +0530

    jit commit final

---
+++ src/testCases/subMission.java
@@ -0,0 +1,11 @@
+package testCases;
+ 
+public class subMission {
+	public static void main(String[]args) {
+		System.out.println(" hello world");
+		System.out.println(" hello world11111111111");
+		System.out.println(" hello world22222222222222222222222");
+
+	}
+
+}
